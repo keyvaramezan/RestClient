b92f07d979ea2969b7cefbbe065c72e28e4f74a7
keyvan_ramezan <keyvanramezan@gmail.com>
2022-01-14 18:56:35 +0330

    add new features

---
+++ Shared/AppBar.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace RestClient.Shared
+{
+    public partial class AppBar
+    {
+        [Parameter]
+        public EventCallback OnSidebarToggled { get; set; }
+        public async Task ToggleSidebar()
+        {
+            await OnSidebarToggled.InvokeAsync();
+        }
+        [Parameter]
+        public EventCallback OnThemeToggled { get; set; }
+
+        public async Task ToggleTheme()
+        {
+            themeMode = !themeMode;
+            await OnThemeToggled.InvokeAsync();
+        }
+        private bool themeMode = false;
+        public string ThemeIcon()
+        {
+            return themeMode ? Icons.Material.Filled.Brightness5 : Icons.Material.Filled.Brightness4;
+        }
+
+    }
+}
